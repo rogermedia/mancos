4193d6f615b9b8da94266959648960527ea35062
Roger Williams <roger@rogerwilliamsmedia.com>
2018-08-12 16:30:02 -0600

    Updated Theme name to mancos

--- dev/config/themeConfig.js
+++ dev/config/themeConfig.js
@@ -2,9 +2,9 @@
 
 module.exports = {
 	theme: {
-		slug: 'wprig',
-		name: 'WP Rig',
-		author: 'Morten Rand-Hendriksen'
+		slug: 'mancos',
+		name: 'Mancos',
+		author: 'Roger Williams'
 	},
 	dev: {
 		browserSync: {
